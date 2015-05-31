5037d26b984dcacf6e81972259b9688219f73c8a
denneulin <a.denneulin@gmail.com>
2015-05-31 19:27:52 +0200

    some tests with mocha and tests coverage with istanbul

--- test/server/kid.test.js
+++ test/server/kid.test.js
@@ -1,1 +1,155 @@
-"use strict";
+'use strict';
+
+
+/**
+ * REST API Tests : Kid
+ */
+var request = require('supertest');
+var app = require('../../server/server.js');
+var should = require('should');
+var async = require('async');
+
+before(function importSampleData(done) {
+  this.timeout(50000);
+  if (app.importing) {
+    app.on('import done', done);
+  } else {
+    done();
+  }
+});
+
+
+function json(verb, url) {
+  return request(app)[verb](url)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/);
+}
+
+describe('REST', function() {
+
+  var token;
+  var memberId;
+  var kidId;
+  var relativeId;
+  var credentials = {username:'parent1', password:'toto'};
+  var member = {
+    firstname: 'parent1',
+    lastname: 'parent1',
+    gender: 'F',
+    phoneNumber: '98765456789',
+    username: credentials.username,
+    email: 'parent1@gmail.com'
+  };
+
+  var kid = {
+    firstname: 'enfant1'
+  };
+
+
+  describe('/api/kids', function () {
+
+    before(function(done) {
+      member.password = credentials.password;
+      json('post', '/api/members')
+        .send(member)
+        .expect(200)
+        .end(function (err, res) {
+          delete member.password;
+          if(err) return done(err);
+
+          json('post', '/api/members/login')
+            .send(credentials)
+            .expect(200)
+            .end(function (err, res) {
+              if(err) return done(err);
+              token = res.body;
+              memberId = token.userId;
+              done();
+            });
+        });
+    });
+
+    describe('Expected Usage', function() {
+      it('should create a new kid on POST /api/members/{id}/kids', function (done) {
+        json('post', '/api/members/' + memberId + '/kids')
+          .set('Authorization', token.id)
+          .send(kid)
+          .expect(200)
+          .end(function (err, res) {
+            if(err) return done(err);
+
+            res.body.should.be.type('object');
+            res.body.should.have.property('id');
+            res.body.should.have.property('firstname', kid.firstname);
+            kidId = res.body.id;
+            done();
+          });
+      });
+
+      it('should define the relation type as parent on PUT /api/relatives/{id}', function(done) {
+        json('get', '/api/relatives/findOne?filter={"where":{"memberId":"' + memberId + '","kidId":"' + kidId + '"}}')
+          .set('Authorization', token.id)
+          .expect(200)
+          .end(function (err, res) {
+            if(err) return done(err);
+
+            res.body.should.be.type('object');
+            res.body.should.have.property('id');
+            res.body.should.have.property('memberId', memberId);
+            res.body.should.have.property('kidId', kidId);
+
+            relativeId = res.body.id;
+            json('put', '/api/relatives/' + relativeId)
+              .set('Authorization', token.id)
+              .send({relationType:'parent'})
+              .expect(200)
+              .end(function (err, res) {
+                if(err) return done(err);
+                res.body.should.be.type('object');
+                res.body.should.have.property('id');
+                res.body.should.have.property('memberId', memberId);
+                res.body.should.have.property('kidId', kidId);
+                res.body.should.have.property('relationType', 'parent');
+
+                done();
+              });
+          });
+      });
+    });
+
+    describe('Unexpected Usage', function() {
+      it('should not create a kid on POST /api/kids', function (done) {
+        json('post', '/api/kids')
+          .set('Authorization', token.id)
+          .send(kid)
+          .expect(404)
+          .end(done);
+      });
+    });
+
+    after(function(done) {
+      async.series([
+        function(cb) {
+          json('delete', '/api/members/' + memberId)
+            .set('Authorization', token.id)
+            .expect(204)
+            .end(cb);
+        },
+        function(cb) {
+          json('delete', '/api/kids/' + kidId)
+            .set('Authorization', token.id)
+            .expect(204)
+            .end(cb);
+        },
+        function(cb) {
+          json('delete', '/api/relatives/' + relativeId)
+            .set('Authorization', token.id)
+            .expect(204)
+            .end(cb);
+        }
+      ], done);
+    });
+  });
+});
+
--- test/server/member.test.js
+++ test/server/member.test.js
@@ -2,11 +2,11 @@
 
 
 /**
- * REST API Tests
+ * REST API Tests : Member
  */
 var request = require('supertest');
 var app = require('../../server/server.js');
-var assert = require('assert');
+var should = require('should');
 
 before(function importSampleData(done) {
   this.timeout(50000);
@@ -27,33 +27,188 @@ function json(verb, url) {
 
 describe('REST', function() {
 
-  describe('test1', function() {
-    it('2+2 = 2', function() {
-      assert.equal(2+2, 4);
-    });
-  });
+  var token;
+  var memberId;
+  var credentials = {username:'parent1', password:'toto'};
+  var member = {
+    firstname: 'parent1',
+    lastname: 'parent1',
+    gender: 'F',
+    phoneNumber: '98765456789',
+    username: credentials.username,
+    email: 'parent1@gmail.com'
+  };
 
-  describe('Expected Usage', function() {
 
-    describe('POST /api/parents', function () {
-      it('should create a new parent', function (done) {
-        json('post', '/api/parents')
-          .send({
-            firstname: 'parent1',
-            lastname: 'parent1',
-            phoneNumber: '98765456789',
-            username: 'parent1',
-            email: 'parent@gmail.com',
-            password: 'toto'
-          })
+  //TODO : création avec mauvaises données, ...
+  //TODO : confirm email
+  //TODO : change pwd
+  describe('/api/members', function () {
+    describe('Unexpected Usage', function() {
+      before(function(done) {
+        member.password = credentials.password;
+        json('post', '/api/members')
+          .send(member)
           .expect(200)
           .end(function (err, res) {
-            assert(typeof res.body === 'object');
-            assert(res.body.id, 'must have an id');
-            assert(res.body.username === 'parent1');
-            //TODO check password absent, id ok etc.
+            delete member.password;
+            done(err);
+          });
+      });
+      it('should not create a new member with duplicate email on POST /api/members', function (done) {
+        json('post', '/api/members')
+          .send({
+            firstname: 'parent2',
+            lastname: 'parent2',
+            gender: 'F',
+            phoneNumber: '9876A5456789',
+            username: 'parent2',
+            email: member.email,
+            password: 'toto'
+          })
+          .expect(422)
+          .end(done);
+      });
+      it('should not create a new member with duplicate phoneNumber on POST /api/members', function (done) {
+        json('post', '/api/members')
+          .send({
+            firstname: 'parent2',
+            lastname: 'parent2',
+            gender: 'F',
+            phoneNumber: member.phoneNumber,
+            username: 'parent2',
+            email: 'parent2@gmail.com',
+            password: 'toto'
+          })
+          .expect(422)
+          .end(done);
+      });
+      it('should not create a new member with duplicate username on POST /api/members', function (done) {
+        json('post', '/api/members')
+          .send({
+            firstname: 'parent2',
+            lastname: 'parent2',
+            gender: 'F',
+            phoneNumber: '987612315456789',
+            username: member.username,
+            email: 'parent2@gmail.com',
+            password: 'toto'
+          })
+          .expect(422)
+          .end(done);
+      });
 
+      after(function(done) {
+        json('post', '/api/members/login')
+          .send(credentials)
+          .expect(200)
+          .end(function (err, res) {
+            if(err) return done(err);
+
+            json('delete', '/api/members/' + res.body.userId)
+              .set('Authorization', res.body.id)
+              .expect(204)
+              .end(done);
+          });
+      });
+    });
+
+    describe('Expected Usage', function() {
+      it('should create a new member on POST /api/members', function (done) {
+        member.password = credentials.password;
+        json('post', '/api/members')
+          .send(member)
+          .expect(200)
+          .end(function (err, res) {
+            delete member.password;
+            if(err) return done(err);
+            res.body.should.be.type('object');
+            res.body.should.have.property('id');
+            res.body.should.have.property('username', 'parent1');
+            res.body.should.not.have.property('password');
             done();
+          });
+      });
+
+      //TODO confirmation du user
+      it('should login the member with username/password on POST /api/members/login', function (done) {
+        json('post', '/api/members/login')
+          .send(credentials)
+          .expect(200)
+          .end(function (err, res) {
+            if(err) return done(err);
+            res.body.should.be.type('object');
+            res.body.should.have.property('id');
+            res.body.should.have.property('userId');
+            token = res.body;
+            memberId = token.userId;
+            done();
+          });
+      });
+      it('should allow GET /api/members/{my-id}', function(done) {
+        json('get', '/api/members/' + memberId)
+          .set('Authorization', token.id)
+          .expect(200, function(err, res) {
+            if (err) return done(err);
+            res.body.should.have.properties(member);
+            res.body.should.not.have.property('password');
+            done();
+          });
+      });
+      it('should not allow GET /api/members/{my-id} without token', function(done) {
+        json('get', '/api/members/' + memberId)
+          .expect(401, done);
+      });
+      it('should not allow GET /api/members/{another-id}', function(done) {
+        json('get', '/api/members/' + (memberId + 1000))
+          .set('Authorization', token.id)
+          .expect(401, done);
+      });
+
+      it.skip('should not logout member on POST /api/members/logout without token', function(done) {
+        json('post', '/api/members/logout')
+          .expect(401, done); // TODO check why error 500 instead of 401
+      });
+      it('should logout existing member on POST /api/members/logout', function(done) {
+          json('post', '/api/members/logout')
+            .set('Authorization', token.id)
+            //.send({})
+            .expect(204, done);
+        }
+      );
+      it('should login the member with email/password on POST /api/members/login', function (done) {
+        json('post', '/api/members/login')
+          .send({email:'parent1@gmail.com', password:'toto'})
+          .expect(200)
+          .end(function (err, res) {
+            if(err) return done(err);
+            res.body.should.be.type('object');
+            res.body.should.have.property('id');
+            res.body.should.have.property('userId');
+            token = res.body;
+            memberId = token.userId;
+            done();
+          });
+      });
+      it('should logout existing member on POST /api/members/logout',
+        function(done) {
+          json('post', '/api/members/logout')
+            .set('Authorization', token.id)
+            .expect(204, done);
+        }
+      );
+
+      after(function(done) {
+        json('post', '/api/members/login')
+          .send(credentials)
+          .expect(200)
+          .end(function (err, res) {
+            if(err) return done(err);
+
+            json('delete', '/api/members/' + res.body.userId)
+              .set('Authorization', res.body.id)
+              .expect(204)
+              .end(done);
           });
       });
     });
